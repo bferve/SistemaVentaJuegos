d2fb73023985d172f4f085af3e6b128a72abe960
Boris Velizz <velizvelizb@gmail.com>
2025-04-08 11:46:55 -0400

    Nueva Clase Prueba

---
+++ src/main/java/ClasePueba.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Boris Veliz
+ */
+public class ClasePueba {
+    public void suma(){
+        System.out.println(1+1);
+    }
+}
